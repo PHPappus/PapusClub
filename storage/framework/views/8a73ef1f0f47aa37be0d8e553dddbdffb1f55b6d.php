15c2fc252588212a3605fb3264e63b506b1a9f26
Juani <juanchichu>
2016-05-06 11:42:25 -0500

    borrando cache vistas viejas

--- storage/framework/views/8a73ef1f0f47aa37be0d8e553dddbdffb1f55b6d.php
+++
@@ -1,8 +0,0 @@
-
-<?php if(Session::has('message-error')): ?>
-<div class="alert alert-danger alert-dismissible" role="alert">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  <?php echo e(Session::get('message-error')); ?>
-
-</div>
-<?php endif; ?>
