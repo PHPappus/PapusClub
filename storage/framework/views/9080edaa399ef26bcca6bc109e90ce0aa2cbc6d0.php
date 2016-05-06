fa04169981e377f660088537425aeb832b879526
Juani <juanchichu>
2016-05-06 11:17:02 -0500

    borrando estoy

--- storage/framework/views/9080edaa399ef26bcca6bc109e90ce0aa2cbc6d0.php
+++ storage/framework/views/9080edaa399ef26bcca6bc109e90ce0aa2cbc6d0.php
@@ -9,49 +9,49 @@
 	<link rel="stylesheet" href="css/font-awesome.css">
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/MisEstilos.css">
+	<!-- <link rel="stylesheet" type="text/css" href="css/estilos.css"> -->
 	
 </head>
 <body>
-@extends('layouts.headerandfooter-al-admin')
-@section('content')
+<!--Cabecera-->
+
+<?php $__env->startSection('content'); ?>
 <!---Cuerpo -->
 
 <main class="main">
 	<div class="content" style="max-width: 100%;">
-	<!-- Utilizando Bootstrap -->
-	<br/><br/>
-	<div class="container">
-		<div class="row">
-		<!-- col-sm-4 crea 3 columnas del mismo tamaño -->
-  			<div class="col-sm-4 nopadding">
-  			<!-- bxslider es un plugin que permite crear sucesión de imagenes -->
-                <ul class="bxslider">
-                    <li><img class="slider img-responsive" src="images/rugby.jpg" /></li>
-                    <li><img class="slider img-responsive" src="images/tennis.jpg" /></li>
-                    <li><img class="slider img-responsive" src="images/remo.jpg" /></li>
-                </ul>  						
-  			</div>
-  			<div class="col-sm-4 nopadding">
-            	<ul class="bxslider">
-               		<li><img class="slider" src="images/bungalows-1.jpg" /></li>
-                	<li><img class="slider" src="images/bungalows-2.jpg" /></li>
-                	<li><img class="slider" src="images/bungalows-3.jpg" /></li>
-            	</ul>  						 			
-  			</div>
-  			<div class="col-sm-4 nopadding">
-            	<ul class="bxslider">
-                	<li><img class="slider" src="images/ajedrez.jpg" /></li>
-                	<li><img class="slider" src="images/teatro.jpg" /></li>
-                	<li><img class="slider" src="images/contradanza.jpg" /></li>
-            	</ul>   			
-  			</div>
-		</div>
-	</div>
-		
+		<!-- Utilizando Bootstrap -->
+		<br/><br/>
+		<div class="container">
+			<div class="row">
+			<!-- col-sm-4 crea 3 columnas del mismo tamaño -->
+	  			<div class="col-sm-4 nopadding">
+	  			<!-- bxslider es un plugin que permite crear sucesión de imagenes -->
+	                <ul class="bxslider">
+	                    <li><img class="slider img-responsive" src="images/rugby.jpg" /></li>
+	                    <li><img class="slider img-responsive" src="images/tennis.jpg" /></li>
+	                    <li><img class="slider img-responsive" src="images/remo.jpg" /></li>
+	                </ul>  						
+	  			</div>
+	  			<div class="col-sm-4 nopadding">
+	            	<ul class="bxslider">
+	               		<li><img class="slider" src="images/bungalows-1.jpg" /></li>
+	                	<li><img class="slider" src="images/bungalows-2.jpg" /></li>
+	                	<li><img class="slider" src="images/bungalows-3.jpg" /></li>
+	            	</ul>  						 			
+	  			</div>
+	  			<div class="col-sm-4 nopadding">
+	            	<ul class="bxslider">
+	                	<li><img class="slider" src="images/ajedrez.jpg" /></li>
+	                	<li><img class="slider" src="images/teatro.jpg" /></li>
+	                	<li><img class="slider" src="images/contradanza.jpg" /></li>
+	            	</ul>   			
+	  			</div>
+			</div>
+		</div>			
 		<div class="container">
 			<div class="content">
 				<div class="row">
-					
 							<a class="convenios col-sm-2 text-center" href="#" title="Convenios">
 								<img alt="convenio" src="images/conection.png">CONVENIOS
 							</a>					
@@ -59,8 +59,6 @@
 							<a class="calendario col-sm-2 text-center" href="#" title="calendario">
 								<img alt="calendario" src="images/calendar.png">CALENDARIO
 							</a>
-	 			
-				
 							<a class="concesiones col-sm-3 text-center" href="#" title="concesiones">
 								<img alt="concesiones" src="images/restaurante.png">CONCESIONES
 							</a>
@@ -68,21 +66,14 @@
 							<a class="galeria col-sm-2 text-center" href="#" title="galeria">
 								<img alt="galeria" src="images/image.png">GALERÍA
 							</a>
-				
-	 		
 							<a class="informes col-sm-3 text-center" href="#" title="informes">
 								<img alt="informes" src="images/person.png">INFORMES ASOCIADOS
 							</a>
-	 					
 				</div>
 			</div>
-				
-			</div>
-		</div>		
-</main>
-
-<!--Pie de págna-->
-@stop
+		</div>
+	</div>		
+<?php $__env->stopSection(); ?>
 <!-- JQuery -->
 	<script src="js/jquery-1.11.3.min.js"></script>
 	<!-- Bootstrap -->
@@ -95,3 +86,4 @@
 
 </body>
 </html>
+<?php echo $__env->make('layouts.headerandfooter', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
