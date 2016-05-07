e9c10d7935ce49371e96031a685ed2aa2afa0a3b
JLoayza <loayza.juan@pucp.pe>
2016-05-06 19:48:14 -0500

    cambios controller

--- app/Http/Controllers/PostulanteController.php
+++ app/Http/Controllers/PostulanteController.php
@@ -13,13 +13,13 @@ class PostulanteController extends Controller
 {
     public function index()
     {
-    	$sedes = Sede::all();
-    	return view('admin-general.sede.index', compact('sedes'));
+    	$sedes = Postulante::all();
+    	return view('registrar-postulante', compact('postulante'));
     }
 
     public function create()
     {
-    	return view('admin-general.sede.newSede');
+    	return view('registrar-postulante');
     }
 
     public function store(StorePostulanteRequest $request)
@@ -27,24 +27,47 @@ class PostulanteController extends Controller
     	$input = $request->all();
 
     	$postulante = new Postulante();
-    	
+    	$postulante->nombre = $input['nombre'];
+    	$postulante->apellido = $input['apellido'];
+    	$postulante->tipoDoc = $input['tipoDoc'];
+    	$postulante->docId = $input['docId'];
+    	$postulante->fechaNac = $input['fechaNac'];
+    	$postulante->estadoCiv = $input['estadoCiv'];
+    	$postulante->lugarNac = $input['lugarNac'];
+    	$postulante->nacionalidad = $input['nacionalidad'];
+    	$postulante->direccion = $input['direccion'];
+    	$postulante->telefono = $input['telefono'];
+    	$postulante->celular = $input['celular'];
+    	$postulante->correo = $input['correo'];
+    	$postulante->nivelIns = $input['nivelIns'];
+    	$postulante->colegio = $input['colegio'];
+    	$postulante->carrera = $input['carrera'];
+    	$postulante->centroTrabajo = $input['centroTrabajo'];
+    	$postulante->dirTrab = $input['dirTrab'];
+    	$postulante->telfTrab = $input['telfTrab'];
+    	$postulante->cargo = $input['cargo'];
+    	$postulante->nombCon = $input['nombCon'];
+    	$postulante->apCon = $input['apCon'];
+    	$postulante->tDocCon = $input['tDocCon'];
+    	$postulante->docCon = $input['docCon'];
+    	$postulante->fechaNacCon = $input['fechaNacCon'];
+    	$postulante->estCivCon = $input['estCivCon'];
+    	$postulante->nombP = $input['nombP'];
+    	$postulante->apP = $input['apP'];
+    	$postulante->tDocP = $input['tDocP'];
+    	$postulante->docP = $input['docP'];
+    	$postulante->fechaNacP = $input['fechaNacP'];
+    	$postulante->estCivP = $input['estCivP'];
+    	$postulante->nombM = $input['nombM'];
+    	$postulante->apM = $input['apM'];
+    	$postulante->tDocM = $input['tDocM'];
+    	$postulante->docM = $input['docM'];
+    	$postulante->fechaNacM = $input['fechaNacM'];
+    	$postulante->estCivM = $input['estCivM'];
 
     	$postulante->save();
     	return back();
     	//return $request->all();	
     }
 
-    public function edit($id)
-    {
-        $sede = Sede::find($id);
-        return view('admin-general.sede.editSede', compact('sede'));
-    }
-
-    public function destroy($id)    
-    {
-        $sede = Sede::find($id);
-        //falta ver lo de las relaciones
-        $sede->delete();
-        return back();
-    }
 }
